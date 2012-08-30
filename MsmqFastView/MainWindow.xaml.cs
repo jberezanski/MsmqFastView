e54c20db77e551dd9fb058c6c0178b1d74a701b2
Jakub Berezanski <kuba@berezanscy.pl>
2012-08-31 01:20:07 +0200

    Enabled connecting to remote machines.

--- MsmqFastView/MainWindow.xaml.cs
+++ MsmqFastView/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System.ComponentModel;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Web.Script.Serialization;
 using System.Windows;
 using MsmqFastView.Infrastructure;
@@ -30,5 +32,22 @@ namespace MsmqFastView
             Settings.Default.MainWindowPlacement = new JavaScriptSerializer().Serialize(this.GetPlacement());
             Settings.Default.Save();
         }
+
+		private void OnEditMachineNameIsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+		{
+			if ((bool)e.NewValue)
+			{
+				// must be deferred, otherwise Focus() will not work
+				Task.Factory.StartNew(
+					() =>
+					{
+						this.tbCandidateMachineName.Focus();
+						this.tbCandidateMachineName.SelectAll();
+					}, 
+					CancellationToken.None, 
+					TaskCreationOptions.None, 
+					TaskScheduler.FromCurrentSynchronizationContext());
+			}
+		}
     }
 }
